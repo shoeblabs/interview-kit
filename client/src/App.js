882b4359d68390e7a572b5b6d2e6a7b507896f65
Shoeb Raza <shoeb@non.sa>
2021-08-17 10:49:19 +0530

    Basic Setup - create react app + tailwind

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="flex flex-col">
+      <header className="flex flex-row justify-between items-center flex-nowrap px-4 py-2 bg-blue-600">
+          <div className="text-white text-2xl">Interview Kit</div>
+          <button className="btn btn-red">Login</button>
+      </header>
+    </main>
+  );
+}
+
+export default App;
